2f404f5e4e5a12cac8f4f1dedaa28277ecd2e253
Joachim Metz <joachim.metz@gmail.com>
2016-01-06 07:27:40 +0100

    Applied updates.

--- libfwps/libfwps_storage.h
+++ libfwps/libfwps_storage.h
@@ -1,7 +1,7 @@
 /*
  * Windows Serialized Property Storage functions
  *
- * Copyright (C) 2013-2015, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2013-2016, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
